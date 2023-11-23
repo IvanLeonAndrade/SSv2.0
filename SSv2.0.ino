e126d402be80764817e736873cd4f6057063ebb0
Ivan Andrade <109771603+IvanLeonAndrade@users.noreply.github.com>
2023-11-23 00:04:52 -0300

    Update SSv2.0.ino
    Add move sensor

--- SSv2.0.ino
+++ SSv2.0.ino
@@ -1,13 +1,13 @@
 //  Sistema de Sensado (SSv1.0)
 /*
- *  Copyright (C) 2023  Ivan Leon Andrade Franco
- *  Empresa: Silica Networks SA
- *
- *  Sensa temperatura, humedad relativa, corriente continua,  
- *  tension y detecta movimiento con sensor infrarojo con el 
- *  protocolo SNMP v1 
-  */
-  
+    Copyright (C) 2023  Ivan Leon Andrade Franco
+    Empresa: Silica Networks SA
+
+    Sensa temperatura, humedad relativa, corriente continua,
+    tension y detecta movimiento con sensor infrarojo con el
+    protocolo SNMP v1
+*/
+
 #include <SPI.h>
 #include <Ethernet.h>
 #include <Streaming.h>
@@ -16,18 +16,32 @@
 #include <Agentuino.h>
 #include <DHT.h>
 
-// cs: Variable relacionada con el sensor de corriente (Current Sensor)
+/* Los siguientes terminos indican 
+ * que variables pertenecen a cada sensor
+ * CS: Current Sensor
+ * VS: Voltage Sensor
+ * MS: Move Sensor
+*/
 const uint8_t  dht_pin = 5;
-const uint8_t  cs_pin = 0; 
+const uint8_t  cs_pin = 0;
+const uint8_t  vs_pin = 0;
+const uint8_t  ms_pin = 3;
+bool ms_state = false;
+
 
 // Las magnitudes se envian con solicitudes SNMP (GET)
 float corriente = 0.0;
 float humedad = 0.0;
 float temperatura = 0.0;
 
+
 // Calibra CS
-const float cs_tension_offset = 2475.0; 
+const float cs_tension_offset = 2475.0;
 const float cs_sensitivity = 12.0; //  [mV]
+
+// Tiempo alarmado del ms
+const unsigned long ms_tiempo_alarmado = 360000; // Tiempo en milisegundos para mantener el valor en 1
+unsigned long ms_tiempo_inicial = 0;
 
 DHT dht(dht_pin, DHT22);
 
@@ -48,6 +62,7 @@ const char sysServices[] PROGMEM   = "1.3.6.1.2.1.1.7.0";
 const char snmp_temperature[]     PROGMEM     = "1.3.6.1.3.2016.5.1.0";
 const char snmp_humidity[]        PROGMEM     = "1.3.6.1.3.2016.5.1.1";
 const char snmp_corriente[]        PROGMEM     = "1.3.6.1.3.2016.5.1.2";
+const char snmp_movimiento[]        PROGMEM     = "1.3.6.1.3.2016.5.1.4";
 
 static char locDescr[35]            = "Sistema de Sensado v1.0";
 static char locContact[25]          = "Silica Networks SA";
@@ -60,16 +75,16 @@ char oid[SNMP_MAX_OID_LEN];
 SNMP_API_STAT_CODES api_status;
 SNMP_ERR_CODES status;
 
-/** 
- *  calculo_corriente - promedio
- *  @cs_sensitivity: Razon de la lectura de tension del cs por Amper (12mV/A)
- *  @cs_tension_offset: Centra la tension de lectura en cero
- *  
- *  Calcula y promedia el resultado de la corriente [A].
- *  
- *  cs entrega un rango de tension de 0 a 5 en [V]. Con 2.5V, 0A; 5V, 210A; 0V, -210A. Tension 
- *  de lectura > 2.5v, es corriente positiva, caso contrario, corriente negativa.
- */ 
+/**
+    calculo_corriente - promedio
+    @cs_sensitivity: Razon de la lectura de tension del cs por Amper (12mV/A)
+    @cs_tension_offset: Centra la tension de lectura en cero
+
+    Calcula y promedia el resultado de la corriente [A].
+
+    cs entrega un rango de tension de 0 a 5 en [V]. Con 2.5V, 0A; 5V, 210A; 0V, -210A. Tension
+    de lectura > 2.5v, es corriente positiva, caso contrario, corriente negativa.
+*/
 float calculo_corriente() {
   float cs_suma = 0.0;
   float cs_tension = 0.0;
@@ -83,13 +98,28 @@ float calculo_corriente() {
   return cs_suma / 4.0;
 }
 
+// Detecta movimiento
+
+int detectar_movimiento() {
+  if (digitalRead(ms_pin) == HIGH && !ms_state) {
+      ms_state = true;
+      ms_tiempo_inicial = millis();  
+  } 
+
+  if (ms_state && millis() - ms_tiempo_inicial > ms_tiempo_alarmado) { // Si ha pasado el tiempo, resetea el valor a 0
+    ms_state = false;
+  }
+  delay(100); 
+}
+
+
 /** pduReceived
- *  Maneja solicitudes SNMP. Para los OID, responde
- *  con mensaje de error: READ_ONLY, si la solicitud SNMP
- *  es SET.
- *  
- *  Nota: No pueden enviarse TRAPS.
- */
+    Maneja solicitudes SNMP. Para los OID, responde
+    con mensaje de error: READ_ONLY, si la solicitud SNMP
+    es SET.
+
+    Nota: No pueden enviarse TRAPS.
+*/
 void pduReceived() {
   SNMP_PDU pdu;
   api_status = Agentuino.requestPdu(&pdu);
@@ -198,7 +228,20 @@ void pduReceived() {
         pdu.type = SNMP_PDU_RESPONSE;
         pdu.error = status;
       }
-    } 
+    } else if ( strcmp_P(oid, snmp_movimiento ) == 0 ) {
+      // handle sysName (set/get) requests
+      if ( pdu.type == SNMP_PDU_SET ) {
+
+        pdu.type = SNMP_PDU_RESPONSE;
+        pdu.error = SNMP_ERR_READ_ONLY;
+      }
+      else {
+        int ms_state_value = ms_state ? 1 : 0;
+        status = pdu.VALUE.encode(SNMP_SYNTAX_OCTETS, dtostrf(ms_state_value, 6, 2, result));
+        pdu.type = SNMP_PDU_RESPONSE;
+        pdu.error = status;
+      }
+    }
     else {
       pdu.type = SNMP_PDU_RESPONSE;
       pdu.error = SNMP_ERR_NO_SUCH_NAME;
@@ -210,18 +253,17 @@ void pduReceived() {
 
 
 void setup() {
-  dht.begin();
   Ethernet.begin(mac, ip, ip_dns, ip_gateway, subnet);
-  // Inicia agente SNMP
-  api_status = Agentuino.begin();
-
+  api_status = Agentuino.begin(); // Inicia agente SNMP
+  dht.begin();
+  
+  pinMode(ms_pin, INPUT);
   // Verificación estado agente SNMP
   if ( api_status == SNMP_API_STAT_SUCCESS ) {
     Agentuino.onPduReceive(pduReceived);
     delay(10);
     return;
   }
-  
   delay(10);
 }
 
@@ -233,6 +275,7 @@ void loop() {
     temperatura = dht.readTemperature();
     humedad = dht.readHumidity();
     corriente = calculo_corriente();
+    detectar_movimiento();
     
     prevMillis = millis();
   }
